df082fcbd99fbf06200f0e5851302bb33b20f5ff
Geoffrey Tomlinson <tomlingb@rose-hulman.edu>
2021-10-29 18:31:55 +0000

    updating blynk functionality

--- Oven.py
+++ Oven.py
@@ -30,37 +30,43 @@ if(BLYNK_AUTH == ""):
 # Initialize Blynk
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
+#turn the powerswitch on/off
 @blynk.handle_event('write V0')
 def my_write_handler(pin, value):
-    print('Current V{} value: {}'.format(pin, value[0]))
     if(int(value[0]) == 0):
         On = 1
+        print("Turning power off")
     if(int(value[0]) == 1):
         On = 0
+        print("Turning power on")
     GPIO.output(LED, int(On))
     
+#quit the program
 @blynk.handle_event('write V1')
 def my_write_handler2(pin, value):
     print('Closing the app')
+    GPIO.output(LED, 1)
     exit()
     
+#read the current temperature
 @blynk.handle_event('read V2')
 def my_read_handler(pin):
     print("ping")
     i = GPIO.input(button)
     blynk.virtual_write(2, 1100)
     
+#set the desired temperature
 @blynk.handle_event('write V3')
 def my_write_handler3(pin, value):
-    tempGoal = int(value)
-    pid.setpoint = tempGoal
-    print("Temperature set to: {}".format(tempGoal))
+    # tempGoal = value
+    # pid.setpoint = tempGoal
+    print("Temperature set to: {}".format(value))
     
 while True:
     blynk.run()
     if(start):
         blynk.virtual_write(3, 0)
         start = False
-    output = pid(tempCurrent)
-    print(output)
+    # output = pid(tempCurrent)
+    # print(output)
     

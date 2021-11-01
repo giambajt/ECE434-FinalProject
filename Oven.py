710b902ba6a1dedc4215f3d9daac3b3978b4c9a5
Geoffrey Tomlinson <tomlingb@rose-hulman.edu>
2021-11-01 15:00:48 +0000

    updated Oven.py

--- Oven.py
+++ Oven.py
@@ -6,7 +6,7 @@ import Adafruit_BBIO.GPIO as GPIO
 from simple_pid import PID
 
 pid = PID(1, 0.1, 0.05, setpoint=1)
-pid.sample_time = 0.1
+pid.sample_time = 1
 
 LED = 'P9_14'
 GPIO.setup(LED, GPIO.OUT)
@@ -19,6 +19,7 @@ On = 1
 tempCurrent = 1000
 tempGoal = 0
 start = True
+outputPrev = 0.0
 
 # Get the autherization code (See setup.sh)
 # BLYNK_AUTH = os.getenv('BLYNK_AUTH', default="")
@@ -59,14 +60,21 @@ def my_read_handler(pin):
 @blynk.handle_event('write V3')
 def my_write_handler3(pin, value):
     # tempGoal = value
-    # pid.setpoint = tempGoal
-    print("Temperature set to: {}".format(value))
+    pid.setpoint = int(value[0])
+    print("Temperature set to: {}".format(value[0]))
     
 while True:
     blynk.run()
     if(start):
         blynk.virtual_write(3, 0)
         start = False
-    # output = pid(tempCurrent)
-    # print(output)
+    #tempCurrent = tempCurrent + 1
+    output = pid(tempCurrent)
+    if (output != outputPrev):
+        print(output)
+    outputPrev = output
     
+    #output is all about the difference in values produced by the pid
+    #if there is a difference between two values of 10 or more then it should be on constantly
+    #if there is a difference of -10  or less then it should be off constantly 
+    #if there is a difference of 0 then it should be on and off for equal amounts of time

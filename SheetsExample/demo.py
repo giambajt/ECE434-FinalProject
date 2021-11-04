6a95b314d28366b3d284d5f0899ee8aad6975e13
Demo User <debian@beaglebone.localdomain>
2021-11-04 18:59:30 +0000

    Fixed bug in demo for logging

--- SheetsExample/demo.py
+++ SheetsExample/demo.py
@@ -23,10 +23,10 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import time, sys
 import smbus
+os.system('./setup.sh')
 tmp = smbus.SMBus(1)
 addr = 0x4a
 output = tmp.read_byte_data(addr, 0)
-os.system('./setup.sh')
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 

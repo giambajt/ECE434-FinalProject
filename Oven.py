a717163ae8aa05346f7973fa51fc5dfcb6c1950f
Geoffrey Tomlinson <tomlingb@rose-hulman.edu>
2021-11-08 21:01:07 +0000

    updating Oven.py

--- Oven.py
+++ Oven.py
@@ -3,23 +3,30 @@
 import blynklib
 import os, sys
 import Adafruit_BBIO.GPIO as GPIO
-from simple_pid import PID
+import Adafruit_BBIO.SPI as SPI
+import Adafruit_MAX31855.MAX31855 as MAX31855
 
-pid = PID(1, 0.1, 0.05, setpoint=1)
-pid.sample_time = 1
+# Set up pins for PowerSwitch Tail 2
+Switch = 'P9_14'
+GPIO.setup(Switch, GPIO.OUT)
+GPIO.output(Switch, 1)
 
-LED = 'P9_14'
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED, 1) 
+# Set up the Pins for SPI
+CLK = 'P9_12'
+CS  = 'P9_15'
+DO  = 'P9_23'
+sensor = MAX31855.MAX31855(CLK, CS, DO)
 
-button = 'P9_11'
-GPIO.setup(button, GPIO.IN)
-
+# Create variables
 On = 1
-tempCurrent = 1000
-tempGoal = 0
+CurrentTemp = int(sensor.readTempC())
 start = True
-outputPrev = 0.0
+Goal = 0
+GoalHolder = 0
+GoalPrev = 0
+PrevTemp = 0
+count = 50
+degree_sign = u"\N{DEGREE SIGN}"
 
 # Get the autherization code (See setup.sh)
 # BLYNK_AUTH = os.getenv('BLYNK_AUTH', default="")
@@ -34,47 +41,52 @@ blynk = blynklib.Blynk(BLYNK_AUTH)
 #turn the powerswitch on/off
 @blynk.handle_event('write V0')
 def my_write_handler(pin, value):
+    global On
     if(int(value[0]) == 0):
         On = 1
         print("Turning power off")
     if(int(value[0]) == 1):
         On = 0
         print("Turning power on")
-    GPIO.output(LED, int(On))
+    GPIO.output(Switch, int(On))
     
 #quit the program
 @blynk.handle_event('write V1')
 def my_write_handler2(pin, value):
-    print('Closing the app')
-    GPIO.output(LED, 1)
+    print('\nClosing the app')
+    GPIO.output(Switch, 1)
     exit()
     
 #read the current temperature
 @blynk.handle_event('read V2')
 def my_read_handler(pin):
-    print("ping")
-    i = GPIO.input(button)
-    blynk.virtual_write(2, 1100)
+    blynk.virtual_write(2, CurrentTemp)
     
-#set the desired temperature
+# Hold the temporary value set by the numerical input module
 @blynk.handle_event('write V3')
 def my_write_handler3(pin, value):
-    # tempGoal = value
-    pid.setpoint = int(value[0])
-    print("Temperature set to: {}".format(value[0]))
+    global GoalHolder
+    GoalHolder = int(value[0])
+    
+# Set the primary goal temp to the temporary one
+@blynk.handle_event('write V4')
+def my_write_handler4(pin, value):
+    if (int(value[0]) == 1):
+        global Goal 
+        Goal = GoalHolder
     
 while True:
+    # Run the blynk client. Needs to be run every update
     blynk.run()
-    if(start):
-        blynk.virtual_write(3, 0)
-        start = False
-    #tempCurrent = tempCurrent + 1
-    output = pid(tempCurrent)
-    if (output != outputPrev):
-        print(output)
-    outputPrev = output
     
-    #output is all about the difference in values produced by the pid
-    #if there is a difference between two values of 10 or more then it should be on constantly
-    #if there is a difference of -10  or less then it should be off constantly 
-    #if there is a difference of 0 then it should be on and off for equal amounts of time
+    # Set the message template and current temperature and reprint every 50 updates
+    if (count >= 10):
+        CurrentTemp = int(sensor.readTempC())
+        message = "Current Temp: {}{}C  Goal Temp: {}{}C".format(CurrentTemp,degree_sign,Goal,degree_sign)
+        print(message,end="\r") # Print the message on the same line
+        count = 0
+    count = count + 1
+    
+    
+# had to install the MAX31855 library
+# use tmux for long term testing

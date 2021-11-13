ba3df17d45d6744d61f079edd274b53f42eec959
giambajt <giambajt@rose-hulman.edu>
2021-11-13 15:46:12 -0500

    Changed readTemp.py for the SPI sensor

--- SheetsExample/readTemp.py
+++ SheetsExample/readTemp.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
-import smbus
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.SPI as SPI
+import Adafruit_MAX31855.MAX31855 as MAX31855
 
-tmp = smbus.SMBus(1)
-
-addr = 0x49
-output = tmp.read_byte_data(addr, 0)
-print(str((output*1.8) +32))
+CLK = 'P9_12'
+CS = 'P9_15'
+DO = 'P9_23'
+sensor = MAX31855.MAX31855(CLK, CS, DO)
+CurrentTemp = int(sensor.readTempC())
+print(str(CurrentTemp))
 
